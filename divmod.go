53671f2dedd476ab2d99eade7c2c2aa15e89aa57
Asel03 <tulebaevaasel03@gmail.com>
2023-05-26 11:36:27 +0600

    add main.go

--- divmod.go
+++ divmod.go
@@ -1,14 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	n := 0
-	PointOne(&n)
-	fmt.Println(n)
+	a := 13
+	b := 2
+	var div int
+	var mod int
+	DivMod(a, b, &div, &mod)
+	fmt.Println(div)
+	fmt.Println(mod)
 }
-func PointOne(n *int) {
-	*n = 1
+
+/*
+This function will divide the int a and b.
+The result of this division will be stored in the int pointed by div.
+The remainder of this division will be stored in the int pointed by mod.
+*/
+func DivMod(a, b int, div, mod *int) {
+	*div = a / b
+	*mod = a % b
 }
--- ultimatepointone.go
+++ ultimatepointone.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-func UltimatePointOne(n ***int) {
-	***n = 1
-}
+// * pointer
+// & adress
 
+func changeLocal(num *[5]int) { // *
+	num[0] = 55
+
+	fmt.Println("\ninside function:\n ", &num)
+
+}
 func main() {
-	a := 0
-	b := &a
-	n := &b
-	UltimatePointOne(&n)
-	fmt.Println(a)
+	num := [...]int{5, 6, 7, 8, 8}
+	fmt.Println("\nbefore passing to function:\n ", &num)
+
+	changeLocal(&num) //num is passed by value
+	fmt.Println("\nadress of num:\n  ", &num)
+
+	fmt.Println("\nafter passing to function: ", &num)
 }
